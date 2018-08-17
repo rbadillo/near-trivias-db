60c9a2f7950387df89eddf723627dc07a3bb4009
Roberto Badillo <rbadillo@gatech.edu>
2018-08-16 23:11:18 -0400

    adding streaming id

--- 06.sql
+++ 06.sql
@@ -5,6 +5,7 @@ CREATE TABLE `trivias_near`.`trivias_prizes` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '',
   `prize_date` VARCHAR(255) NOT NULL COMMENT '',
   `prize_description` VARCHAR(255) NOT NULL COMMENT '',
+  `streaming_id` VARCHAR(128) NULL COMMENT '',
   `player_winner` VARCHAR(255) NULL COMMENT '',
   `when_created` DATETIME NOT NULL ,
   `when_updated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
